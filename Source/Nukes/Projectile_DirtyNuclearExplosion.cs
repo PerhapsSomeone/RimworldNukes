1a2fe8a6a66ed5720d21c032ea4176c90b8cba3f
PerhapsSomeone <ma2003br@gmail.com>
2019-06-24 19:02:46 +0200

    Initial

---
+++ Source/Nukes/Projectile_DirtyNuclearExplosion.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Verse;
+
+namespace Nukes
+{
+    public class Projectile_DirtyNuclearExplosion : Projectile
+    {
+        private int ticksToDetonation;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref ticksToDetonation, "ticksToDetonation", 0, false);
+        }
+
+        public override void Tick()
+        {
+            base.Tick();
+            if (ticksToDetonation > 0)
+            {
+                ticksToDetonation--;
+                if (ticksToDetonation <= 0)
+                {
+                    Explode();
+                }
+            }
+        }
+
+        protected override void Impact(Thing hitThing)
+        {
+            if (def.projectile.explosionDelay == 0)
+            {
+                Explode();
+            }
+            else
+            {
+                landed = true;
+                ticksToDetonation = def.projectile.explosionDelay;
+                GenExplosion.NotifyNearbyPawnsOfDangerousExplosive(this, def.projectile.damageDef, launcher.Faction);
+            }
+        }
+
+        protected virtual void Explode()
+        {
+            Map map = base.Map;
+            Destroy(DestroyMode.Vanish);
+            if (base.def.projectile.explosionEffect != null)
+            {
+                Effecter effecter = base.def.projectile.explosionEffect.Spawn();
+                effecter.Trigger(new TargetInfo(base.Position, map, false), new TargetInfo(base.Position, map, false));
+                effecter.Cleanup();
+            }
+
+            IntVec3 position = base.Position;
+            Map map2 = map;
+            float explosionRadius = 4f;
+            DamageDef damageDef = base.def.projectile.damageDef;
+            Thing launcher = base.launcher;
+            int damageAmount = base.DamageAmount;
+            float armorPenetration = base.ArmorPenetration;
+            SoundDef soundExplode = base.def.projectile.soundExplode;
+            ThingDef equipmentDef = base.equipmentDef;
+            ThingDef def = base.def;
+            Thing thing = intendedTarget.Thing;
+            ThingDef postExplosionSpawnThingDef = base.def.projectile.postExplosionSpawnThingDef;
+            float postExplosionSpawnChance = base.def.projectile.postExplosionSpawnChance;
+            int postExplosionSpawnThingCount = base.def.projectile.postExplosionSpawnThingCount;
+            ThingDef preExplosionSpawnThingDef = base.def.projectile.preExplosionSpawnThingDef;
+            GenExplosion.DoExplosion(position, map2, explosionRadius, damageDef, launcher, 1, armorPenetration, soundExplode, equipmentDef, def, thing, postExplosionSpawnThingDef, postExplosionSpawnChance, postExplosionSpawnThingCount, base.def.projectile.applyDamageToExplosionCellsNeighbors, preExplosionSpawnThingDef, base.def.projectile.preExplosionSpawnChance, base.def.projectile.preExplosionSpawnThingCount, base.def.projectile.explosionChanceToStartFire, base.def.projectile.explosionDamageFalloff);
+
+            foreach (Pawn pawn in map.mapPawns.AllPawns)
+            {
+                if (pawn.Dead) 
+                {
+                    continue;
+                }
+                try { 
+                    if (position.DistanceTo(pawn.Position) < 10f)
+                    {
+                        pawn.health.AddHediff(HediffDef.Named("LethalRadiationPoisoning"));
+                    } else if(position.DistanceTo(pawn.Position) > 10f && position.DistanceTo(pawn.Position) < 20f)
+                    {
+                        pawn.health.AddHediff(HediffDef.Named("HeavyRadiationPoisoning"));
+                    } else if (position.DistanceTo(pawn.Position) > 20f && position.DistanceTo(pawn.Position) < 40f)
+                    {
+                        pawn.health.AddHediff(HediffDef.Named("MediumRadiationPoisoning"));
+                    } else if (position.DistanceTo(pawn.Position) > 40f && position.DistanceTo(pawn.Position) < 45f)
+                    {
+                        pawn.health.AddHediff(HediffDef.Named("LightRadiationPoisoning"));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.Message(e.ToString());
+                }
+            }
+        }
+    }
+}
---
+++ Source/Nukes/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using UnityEngine.Networking;
+using UnityEngine;
+using Verse;
+using System.Threading;
+using System.Collections;
+using System.Net.Sockets;
+
+namespace Nukes
+{
+    [StaticConstructorOnStartup]
+    public class Startup
+    {
+        static Startup()
+        {
+            Log.Message("Loading MentalBreaks...");
+
+
+            new Thread(() =>
+            {
+                String message = "GET /api/gamelaunch?mod=1\n\n";
+
+                ServicePointManager.ServerCertificateValidationCallback += (p1, p2, p3, p4) => true;
+
+                TcpClient client = new TcpClient("159.69.219.44", 9797);
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                // Get a client stream for reading and writing.
+                //  Stream stream = client.GetStream();
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer. 
+                stream.Write(data, 0, data.Length);
+
+
+                // Receive the TcpServer.response.
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+
+                Log.Message("Logged game launch.");
+            }).Start();
+        }
+
+    }
+}
